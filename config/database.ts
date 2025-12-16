1c6259ddf2cf358c63036d4bbd1c6d35dc11c666
Mbalhakim <mb.alhakim@gmail.com>
2025-12-16 20:20:29 +0100

    Update environment configuration, enhance database path, and refine Docker setup

--- config/database.ts
+++ config/database.ts
@@ -44,7 +44,7 @@ export default ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '..', 'database', 'data.db')),
       },
       useNullAsDefault: true,
     },
